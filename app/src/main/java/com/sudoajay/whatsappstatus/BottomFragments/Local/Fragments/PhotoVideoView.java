4c67c3165eb005f8212412508f1b9d53fd65ee66
sudoajay <sudoajay@gmail.com>
2019-05-26 11:12:52 +0530

    Done

--- app/src/main/java/com/sudoajay/whatsappstatus/BottomFragments/Local/Fragments/PhotoVideoView.java
+++ app/src/main/java/com/sudoajay/whatsappstatus/BottomFragments/Local/Fragments/PhotoVideoView.java
@@ -1,16 +1,24 @@
 package com.sudoajay.whatsappstatus.BottomFragments.Local.Fragments;
 
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
+
 import com.sudoajay.whatsappstatus.R;
 
-public class PhotoVideoView extends Activity {
+public class PhotoVideoView extends AppCompatActivity {
     private PopupWindow mPopupWindow;
     ImageView imageView;
 
@@ -20,93 +28,50 @@ public class PhotoVideoView extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_video_view);
-        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView =  findViewById(R.id.imageView);
 
+        // At First Hide It
+        HideStatusNavigation();
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                HideStatusBar();
 
-//                // Initialize a new instance of LayoutInflater service
-//                LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-//
-//                // Inflate the custom layout/view
-//                View customView = inflater.inflate(R.layout.activity_popup_photo_video_view, null);
-//
-//                /*
-//                    public PopupWindow (View contentView, int width, int height)
-//                        Create a new non focusable popup window which can display the contentView.
-//                        The dimension of the window must be passed to this constructor.
-//
-//                        The popup does not provide any background. This should be handled by
-//                        the content view.
-//
-//                    Parameters
-//                        contentView : the popup's content
-//                        width : the popup's width
-//                        height : the popup's height
-//                */
-//                // Initialize a new instance of popup window
-//                mPopupWindow = new PopupWindow(
-//                        customView,
-//                        ViewPager.LayoutParams.WRAP_CONTENT,
-//                        ViewPager.LayoutParams.WRAP_CONTENT
-//                );
-//
-//                // Set an elevation value for popup window
-//                // Call requires API level 21
-//                if (Build.VERSION.SDK_INT >= 21) {
-//                    mPopupWindow.setElevation(100.0f);
-//                }
-//                mPopupWindow.setAnimationStyle(R.style.Animation);
-//
-//                // Get a reference for the custom view close button
-////                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
-////
-////                // Set a click listener for the popup window close button
-////                closeButton.setOnClickListener(new View.OnClickListener() {
-////                    @Override
-////                    public void onClick(View view) {
-////                        // Dismiss the popup window
-////                        mPopupWindow.dismiss();
-////                    }
-////                });
-//
-//                /*
-//                    public void showAtLocation (View parent, int gravity, int x, int y)
-//                        Display the content view in a popup window at the specified location. If the
-//                        popup window cannot fit on screen, it will be clipped.
-//                        Learn WindowManager.LayoutParams for more information on how gravity and the x
-//                        and y parameters are related. Specifying a gravity of NO_GRAVITY is similar
-//                        to specifying Gravity.LEFT | Gravity.TOP.
-//
-//                    Parameters
-//                        parent : a parent view to get the getWindowToken() token from
-//                        gravity : the gravity which controls the placement of the popup window
-//                        x : the popup's x location offset
-//                        y : the popup's y location offset
-//                */
-//                // Finally, show the popup window at the center location of root relative layout
-//                mPopupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+                if (getWindow().getDecorView().getSystemUiVisibility() == View.SYSTEM_UI_FLAG_VISIBLE) {
+                    HideStatusNavigation();
+                } else {
+                    ShowStatusNavigation();
+                }
+
+                // Set an elevation value for popup window
+                // Call requires API level 21
+                if (Build.VERSION.SDK_INT >= 21) {
+                    mPopupWindow.setElevation(100.0f);
+                }
+                mPopupWindow.setAnimationStyle(R.style.Animation);
             }
         });
 
 
     }
 
-    public void HideStatusBar() {
+    //    Hide Status
+    @SuppressLint("ResourceType")
+    private void HideStatusNavigation() {
         View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
 
-
-//        // Hide status bar
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-// Show status bar
-//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    //    Show Status
+    private void ShowStatusNavigation() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+        decorView.setSystemUiVisibility(uiOptions);
+
+          }
 
 
 }
